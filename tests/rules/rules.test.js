ff496fa88087f32da5a1dce913e1b215704ab660
Ericahyein <ericahyein@users.noreply.github.com>
2026-09-16 13:43:20 +0900

    ci(rules): upgrade to Java 21 for firebase-tools compatibility and sync project ID

--- tests/rules/rules.test.js
+++ tests/rules/rules.test.js
@@ -8,7 +8,7 @@ const {
   assertSucceeds
 } = require('@firebase/rules-unit-testing');
 
-const PROJECT_ID = 'demo-healimbd-rules-test-' + Date.now();
+const PROJECT_ID = process.env.GCLOUD_PROJECT || 'demo-healimbd-rules-test';
 let testEnv;
 
 describe('Healim Firebase Rules Emulator Verification', () => {
